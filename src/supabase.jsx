97585ed0d7ed6c252c15f0289e4546f358b8efcd
martaN7 <vocado7@gmail.com>
2022-10-14 13:31:44 +0200

    variables

--- src/supabase.jsx
+++ src/supabase.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_REACT_APP_SUPABASE_PUBLIC_KEY;
+const supabaseUrl = process.env.VITE_REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.VITE_REACT_APP_SUPABASE_PUBLIC_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
